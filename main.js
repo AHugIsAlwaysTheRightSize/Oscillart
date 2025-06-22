97a418ae0bb1a2cd763b0dd7b21a82e37b8d8e75
AHugIsAlwaysTheRightSize <208357231+AHugIsAlwaysTheRightSize@users.noreply.github.com>
2025-06-22 13:02:22 -0400

    Add record button

--- main.js
+++ main.js
@@ -1,8 +1,9 @@
 const input = document.getElementById('input');
 const color_picker = document.getElementById('color')
 const vol_slider = document.getElementById('vol-slider');
+const recording_toggle = document.getElementById('record');
 
-
+var timepernote = 0;
 
 
 //create web audio api elements
@@ -15,6 +16,10 @@ oscillator.connect(gainNode);
 gainNode.connect(audioCtx.destination);
 oscillator.type = "sine";
 
+oscillator.start();
+gainNode.gain.value = 0;
+
+
 //define canvas variables
 var canvas = document.getElementById("canvas");
 var ctx = canvas.getContext("2d");
@@ -24,20 +29,17 @@ var height = ctx.canvas.height;
 var interval = null;
 var reset = false;
 
-var timepernote = 0;
 var length = 0;
 
-oscillator.start();
-gainNode.gain.value = 0;
 
 notenames = new Map();
-mapName.set("C", 261.6);
-mapName.set("D", 293.7);
-mapName.set("E", 329.6);
-mapName.set("F", 349.2);
-mapName.set("G", 392.0);
-mapName.set("A", 440.0);
-mapName.set("B", 493.9);
+notenames.set("C", 261.6);
+notenames.set("D", 293.7);
+notenames.set("E", 329.6);
+notenames.set("F", 349.2);
+notenames.set("G", 392.0);
+notenames.set("A", 440.0);
+notenames.set("B", 493.9);
 
 
 
@@ -51,7 +53,7 @@ function frequency(pitch) {
     //set frequency pitch to other paramater Oscillator
     oscillator.frequency.setValueAtTime(pitch, audioCtx.currentTime);
 
-    setTimeout(() => { clearInterval(setting); gainNode.gain.value = 0; }, ((timerpernote)-10));
+    setTimeout(() => { clearInterval(setting); gainNode.gain.value = 0; }, ((timepernote)-10));
 
 
     }
@@ -60,21 +62,22 @@ function frequency(pitch) {
 
 
 function handle() {
+    reset = true;
+
     audioCtx.resume();
     gainNode.gain.value = 0;
 
     //drawWave(frequency);
 
-    reset = true;
     
     var usernotes = String(input.value);
-    var noteslist = []
+    var noteslist = [];
 
     length = usernotes.length;
     timepernote = (6000 / length)
 
     for (i = 0; i < usernotes.length; i++) {
-        noteslist.push(notenames.get(usernotes.charAt(i)))
+        noteslist.push(notenames.get(usernotes.charAt(i)));
     }
     
 
@@ -101,7 +104,7 @@ function drawWave() {
         x = 0;
         y = height/2;
         ctx.moveTo(x, y);
-        ctx.beginPath
+        ctx.beginPath();
     }
     counter = 0;
     interval = setInterval(line, 20);
@@ -111,7 +114,7 @@ function drawWave() {
 
 
 function line() {
-    y = height/2 + ((vol_slider.value/100)*40 * Math.sin(x * 2 * Math.PI * freq * (.05*length))); //make sure is actually changed to this
+    y = height/2 + ((vol_slider.value/100)*40 * Math.sin(x * 2 * Math.PI * freq * (.05*length))); 
     ctx.stokeStyle = color_picker.value;
     ctx.lineTo(x, y);
     ctx.stroke();
@@ -120,4 +123,53 @@ function line() {
     if(counter > (timepernote/20)) {
         clearInterval(interval);
     }
+}
+
+
+var blob, recorder = null;
+var chunks = [];
+
+function startRecording(){
+    const canvasStream = canvas.captureStream(20);
+    const audioDestination = audioCtx.createMediaStreamDestination();
+
+
+    const combinedStream = new MediaStream();
+
+    gainNode.connect(audioDestination);
+    canvasStream.getVideoTracks().forEach(track => combinedStream.addTrack(track));
+    audioDestination.stream.getAudioTracks().forEach(track => combinedStream.addTrack(track));
+
+    recorder = new MediaRecorder(combinedStream, { mimeType: 'video/webm' })
+
+
+    recorder.ondataavailable = e => {
+        if (e.data.size > 0) {
+            chunks.push(e.data);
+        }
+    };
+
+    recorder.onstop = () => {
+        const blob = new Blob(chunks, { type: 'video/webm' })
+        const url = URL.createObjectURL(blob);
+        const a = document.createElement('a');
+        a.href = url;
+        a.download = 'recording.webm';
+        a.click();
+        URL.revokeObjectURL(url);
+    };
+
+    recorder.start();
+}
+
+var is_recording = false;
+function toggle() {
+   is_recording = !is_recording;
+   if(is_recording){
+    recording_toggle.innerHTML = "Stop Recording";
+    startRecording();
+   } else {
+    recording_toggle.innerHTML = "Start Recording";
+    recorder.stop();
+   }
 }
